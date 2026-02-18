a5b86c9ccbfd2de68ecfe8868e96988cb4db5e00
Nathaniel Torres <dev.nat.torres@gmail.com>
2026-02-18 21:11:28 +0800

    feat: added initial logic for the chat

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Chat } from '@/components/chat';
 
 export default async function HomePage() {
   return (
-    <div className='max-w-3xl mx-auto flex items-center justify-center'>
+    <div className='max-w-3xl mx-auto flex items-center justify-center py-16'>
       <Chat />
     </div>
   );
--- components/chat/index.tsx
+++ components/chat/index.tsx
@@ -1,6 +1,11 @@
 'use client';
 
-import { useRef } from 'react';
+import { generateAnswer } from '@/actions/chat';
+
+import { useRef, useState, useEffect } from 'react';
+import { toast } from 'sonner';
+import { v4 as uuidv4 } from 'uuid';
+import { cn } from '@/lib/client';
 
 import { PaperclipIcon, SendHorizontalIcon } from 'lucide-react';
 import {
@@ -12,17 +17,92 @@ import {
 
 export function Chat() {
   const attachedFilesRef = useRef<HTMLInputElement>(null);
+  const [hideHeaderText, setHideHeaderText] = useState(false);
+  const [message, setMessage] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [conversation, setConversation] = useState<ChatMessage[]>([]);
+
+  useEffect(() => {
+    alreadySentMessage();
+  }, []);
 
   function simulateUploadClick() {
     attachedFilesRef.current?.click();
   }
 
+  function alreadySentMessage() {
+    if (localStorage.getItem('alreadySentMessage')) setHideHeaderText(true);
+
+    if (conversation.length > 0) setHideHeaderText(false);
+    else setHideHeaderText(true);
+  }
+
+  async function handleSendMessage() {
+    const payload = {
+      id: uuidv4(),
+      message,
+      type: 'user',
+    };
+
+    setIsLoading(true);
+    setMessage('');
+
+    if (!localStorage.getItem('alreadySentMessage')) {
+      localStorage.setItem('alreadySentMessage', 'true');
+      setHideHeaderText(true);
+    }
+
+    try {
+      const response = await generateAnswer(payload);
+
+      if (!response.success) {
+        toast.error(response.message);
+        return;
+      }
+
+      setConversation([...conversation, response.data!]);
+    } catch (err) {
+      console.error(err);
+      toast.error('Failed to send the message.');
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <div className='w-full'>
+      {!hideHeaderText && !isLoading && (
+        <div className='text-center mb-6 space-y-2'>
+          <h3 className='text-3xl font-semibold'>What do you want to know?</h3>
+          <p className='text-sm text-muted-foreground'>
+            Upload files to reference their content and generate appropriate
+            answers.
+          </p>
+        </div>
+      )}
+
+      {!isLoading && hideHeaderText && (
+        <ul className='flex flex-col space-y-4 mb-8 w-full'>
+          {conversation.map((cvx, index) => (
+            <li
+              key={index}
+              className={cn(
+                'w-[80%] border rounded-lg p-4',
+                cvx.type === 'user' ? 'ml-auto' : 'mr-auto',
+              )}
+            >
+              <p>{cvx.message}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+
       <input type='file' ref={attachedFilesRef} multiple className='hidden' />
 
       <InputGroup>
         <InputGroupTextarea
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
           rows={6}
           className='resize-none max-h-32'
           placeholder='Ask questions here...'
@@ -46,6 +126,7 @@ export function Chat() {
             title='Send message'
             aria-label='Send message'
             className='ml-auto'
+            onClick={handleSendMessage}
           >
             <SendHorizontalIcon />
             <p className='hidden lg:block'>Send</p>

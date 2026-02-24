896c33ca04b1b6a574fad8777249556a92e0d17a
Nathaniel Torres <dev.nat.torres@gmail.com>
2026-02-24 19:27:22 +0800

    feat: initialized prisma

--- lib/server/env.ts
+++ lib/server/env.ts
@@ -4,4 +4,6 @@ export const env = cleanEnv(process.env, {
   NODE_ENV: str({ choices: ['development', 'test', 'production'] }),
   GEMINI_API_KEY: str(),
   GEMINI_MODEL: str(),
+  DATABASE_URL: str(),
+  SHADOW_DATABASE_URL: str(),
 });
